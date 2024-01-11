ff41cf32868d872bfda4ce3122fda8f2c1ebc82e
tate <yg3bpwn0Or679Hau8Fxi@duck.com>
2024-01-11 09:01:20 -0600

    redis

--- README.md
+++ README.md
@@ -8,6 +8,12 @@
 
 don't forget ```sudo docker compose down``` after you're done
 
+To remove Docker volumes (postgres volume enabled by default, redis volume disabled by default)
+
+```sudo docker volume rm 495-website_postgres-data```
+
+```sudo docker volume rm 495-website_redis-data```
+
 http://localhost:443/
 
 Not HTTPS, no point in configuring SSL/TLS until first deployment
@@ -15,6 +21,10 @@ Not HTTPS, no point in configuring SSL/TLS until first deployment
 To test JSONReponse/Database:
 
 ```curl localhost:443/db-test-endpt/```
+
+To test Redis:
+
+```curl localhost:443/redis-test-endpt/```
 
 
 Tech stack:
@@ -28,3 +38,5 @@ Reverse proxy: NGINX
 Containerization: Docker/Docker compose
 
 Database: Postgres
+
+Key-Value Store/Message Broker: Redis
--- cs495_website/cs495webapp/views.py
+++ cs495_website/cs495webapp/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import request, JsonResponse, HttpResponse
+from redis import Redis
+
 from etb_db.DB import Db
 from etb_env.ENV import Env
+
 
 env_interface = Env()
 DB_PARAMS = env_interface.get_db_auth()
 db_interface = Db(RDBMS='postgres', AUTH = DB_PARAMS)
 
+red = Redis(host='redis', port=6379)
 
-def hello_world(request):
+
+def hello_world(request: request) -> HttpResponse:
     return render(request, 'examples/hello.html')
 
-def db_test_endpt(request):
+
+def db_test_endpt(request: request) -> JsonResponse:
     try:
         rows = db_interface.get_all(table='example_table')
         return JsonResponse(data = {"response" : rows})
     
     except Exception as e:
-        return JsonResponse(e, 401)
+        return JsonResponse(data = {"error" : e})
+    
+
+def redis_test_endpt(request: request) -> JsonResponse:
+    try:
+        page_hits = red.incr('page_hits')
+        return JsonResponse(data = {"response" : f"Page hits: {page_hits}"})
+    
+    except Exception as e:
+        return JsonResponse(data = {"error" : e})
