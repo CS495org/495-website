f16549b6398de288b0b3e7808053f4c33e61aaf9
rbfisher15 <rbfisher@crimson.ua.edu>
2024-02-06 12:20:38 -0600

    Center scrollable rows and add blur for sidebar

--- template/sketch.js
+++ template/sketch.js
@@ -1,8 +1,11 @@
 //function to bring out side bar when menu button is pressed
 var menu = document.querySelector(".menu-button");
+var overlay = document.querySelector(".overlay");
 
 menu.addEventListener("click", function(event) {
+    event.preventDefault(); //don't scroll back to top when button is pressed
     document.querySelector("body").classList.toggle("active");
+    overlay.style.display = document.querySelector("body").classList.contains("active") ? "block" : "none"; // Toggle overlay display
     event.stopPropagation();
 });  
 
@@ -13,6 +16,7 @@ const toggle = document.getElementById("toggle");
 document.onclick = function(e) {
     if(!e.target.closest(".sidebar")) {
         document.querySelector("body").classList.remove("active");
+        overlay.style.display = "none"; // Hide overlay when sidebar is closed
     }
 
 } 
