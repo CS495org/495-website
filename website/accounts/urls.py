090364f0fbe1776aa6185a99d0279a807b08c6d7
reilandeubank <113558472+reilandeubank@users.noreply.github.com>
2024-02-15 21:45:05 -0600

    Home, Profile, and Discover page are up

--- website/accounts/urls.py
+++ website/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.urls.base import reverse_lazy
 
-from .views import SignUpView
+from .views import SignUpView, profile_view, discover_view
 
 from django.contrib.auth.views import (
     LogoutView, 
@@ -16,29 +16,50 @@ from django.contrib.auth.views import (
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("logout/", LogoutView.as_view(), name="logout"),
-
-    path("password-change/", 
-         PasswordChangeView.as_view(template_name="registration/password_change_form.html",
-                                    success_url = reverse_lazy("password_change_done")), 
-         name="password_change"),
-
-    path("password-change-done/", 
-         PasswordChangeDoneView.as_view(template_name="registration/password_change_done.html"), 
-         name="password_change_done"),
-
-    path('password-reset/', 
-         PasswordResetView.as_view(template_name='registration/password_reset_form.html'), 
-         name='password_reset'),
-
-    path('password-reset/done/', 
-         PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), 
-         name='password_reset_done'),
-
-    path('password-reset-confirm/<uidb64>/<token>/', 
-         PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), 
-         name='password_reset_confirm'),
-
-    path('password-reset-complete/',
-         PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), 
-         name='password_reset_complete'),
+    path("profile", profile_view, name="profile"),
+    path("profile", profile_view, name="profile"),
+    path("discover", discover_view, name="discover"),
+    path(
+        "password-change/",
+        PasswordChangeView.as_view(
+            template_name="registration/password_change_form.html",
+            success_url=reverse_lazy("password_change_done"),
+        ),
+        name="password_change",
+    ),
+    path(
+        "password-change-done/",
+        PasswordChangeDoneView.as_view(
+            template_name="registration/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
+    path(
+        "password-reset/",
+        PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
--- website/accounts/views.py
+++ website/accounts/views.py
@@ -1,9 +1,15 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-
+from django.shortcuts import render
 from .forms import CustomUserCreationForm
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+def profile_view(request):
+    return render(request, "accounts/profile.html")
+
+def discover_view(request):
+    return render(request, "accounts/discover.html")
