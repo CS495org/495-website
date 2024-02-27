58a5dcec7127a71a1f051031726ab9f2196235bc
HFxLhT8JqeU5BnUG <yg3bpwn0or679hau8fxi@duck.com>
2024-02-26 21:05:19 -0600

    chainguard base images for redis and nginx

--- proxy/startup.sh
+++ proxy/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-openssl req -x509 -newkey rsa -keyout /etc/nginx/ssl/key.pem -out /etc/nginx/ssl/cert.pem -days 365 -nodes -config /openssl.conf
+# no longer used
 
-nginx -g "daemon off;"
+# openssl req -x509 -newkey rsa -keyout /etc/nginx/ssl/key.pem -out /etc/nginx/ssl/cert.pem -days 365 -nodes -config /openssl.conf
+
+# nginx -g "daemon off;"
