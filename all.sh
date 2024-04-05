7b140283a015c2d5dadf073e3cde67192d583df2
HFxLhT8JqeU5BnUG <yg3bpwn0or679hau8fxi@duck.com>
2024-04-04 21:18:44 -0500

    cleanup, notes in .env.example

--- all.sh
+++ all.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 docker compose down && docker volume rm 495-website_pg-data #&& docker volume rm 495-website_redis-data
 docker compose up --build --force-recreate --remove-orphans
