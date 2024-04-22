8051a18710e5e824e182eb9dfd9d9260267d47f8
tate <yg3bpwn0Or679Hau8Fxi@duck.com>
2024-04-21 21:26:25 -0500

    forgot to detach

--- all.sh
+++ all.sh
@@ -2,4 +2,4 @@
 
 docker compose down && docker volume rm 495-website_pg-data #&& docker volume rm img-var
 docker volume create img-var
-docker compose up --build --force-recreate --remove-orphans
+docker compose up --build --force-recreate --remove-orphans -d
